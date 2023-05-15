989b9ac0bc32541fa75f3605d5372e5b1329a292
waterh <hwater@alterned.com>
2023-05-15 09:34:08 +0200

    Update again index.php

--- php-test1/index.php
+++ php-test1/index.php
@@ -1,4 +1,4 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello again World! php version is " . PHP_VERSION . "\n";
 print "Hey ! Someone just changed the content ! \n";
 ?>
